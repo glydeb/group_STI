74d379b675b3d8dd719036fbceff77dd5b178c29
Glyde <glydeb@yahoo.com>
2016-04-28 17:09:41 -0500

    convert employees to objects

--- group_01.js
+++ group_01.js
@@ -1,55 +1,68 @@
-var atticus = ["Atticus", "2405", "47000", 3];
-var jem = ["Jem", "62347", "63500", 4];
-var boo = ["Boo", "11435", "54000", 3];
-var scout = ["Scout", "6243", "74750", 5];
+function Person(name, empNumber, salary, rating) {
+  this.name = name;
+  this.empNumber = empNumber;
+  this.salary = salary;
+  this.rating = rating;
+}
+
+var atticus = new Person('Atticus', '2405', '47000', 3);
+var jem = new Person('Jem', '62347', '63500', 4);
+var boo = new Person('Boo', '11435', '54000', 3);
+var scout = new Person('Scout', '6243', '74750', 5);
 
 var employees = [atticus, jem, boo, scout];
 
 function getBonus(employee) {
-    var bonusPercent = 0;
-    var newArray = [];
+  var bonusPercent = 0;
+  var newArray = [];
 
-    newArray[0] = employee[0];
+  newArray[0] = employee.name;
 
-    switch (employee[3]) {
-        case 0:
-        case 1:
-        case 2:
-            break;
-        case 3:
-            bonusPercent = 4;
-            break;
-        case 4:
-            bonusPercent = 6;
-            break;
-        case 5:
-            bonusPercent = 10;
-            break;
-        default:
-            alert("Invalid rating for employee " + employee[0]);
-    }
-    if (employee[1].length === 4) {
-        bonusPercent += 5;
-    }
-    if (employee[2] > 65000) {
-        bonusPercent--;
-    }
-    if (bonusPercent > 13) {
-        bonusPercent = 13;
-    }
-    if (bonusPercent < 0) {
-        bonusPercent = 0;
-    }
+  switch (employee.rating) {
+    case 0:
+    case 1:
+    case 2:
+      break;
+    case 3:
+      bonusPercent = 4;
+      break;
+    case 4:
+      bonusPercent = 6;
+      break;
+    case 5:
+      bonusPercent = 10;
+      break;
+    default:
+      alert('Invalid rating for employee ' + employee.name);
+  }
 
-    newArray[1] = bonusPercent;
-    newArray[2] = (Math.round(employee[2] * ((bonusPercent / 100) + 1) * 100)) / 100;
-    newArray[3] = Math.round(employee[2] * (bonusPercent / 100));
-    return newArray;
+  if (employee.empNumber.length === 4) {
+    bonusPercent += 5;
+  }
+
+  if (employee.salary > 65000) {
+    bonusPercent--;
+  }
+
+  if (bonusPercent > 13) {
+    bonusPercent = 13;
+  }
+
+  if (bonusPercent < 0) {
+    bonusPercent = 0;
+  }
+
+  newArray[1] = bonusPercent;
+  newArray[2] = (Math.round(employee.salary * ((bonusPercent / 100) + 1) * 100)) / 100;
+  newArray[3] = Math.round(employee.salary * (bonusPercent / 100));
+  return newArray;
 }
 
 var outputArray = [];
 
 for (var i = 0; i < employees.length; i++) {
-    outputArray = getBonus(employees[i]);
-    document.getElementById('table').innerHTML += "<tr> <td> " + outputArray[0] + "</td> <td> "+ outputArray[1] + "</td> <td> " + outputArray[2] + "</td><td> " + outputArray[3] + "</td> </tr>";
+  outputArray = getBonus(employees[i]);
+  document.getElementById('table').innerHTML += '<tr> <td> ' + outputArray[0] +
+    '</td> <td> ' + outputArray[1] + '</td> <td> ' + outputArray[2] +
+    '</td><td> ' + outputArray[3] + '</td> </tr>';
 }
